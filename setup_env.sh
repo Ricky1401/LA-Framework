993f2ab2f9401651ebd066e75c408895ba75246a
Rossetto Riccardo <riccardo.rossetto@virgilio.it>
2025-07-31 12:55:38 +0200

    print setup and gitignore

--- setup_env.sh
+++ setup_env.sh
@@ -1,10 +1,16 @@
 export NCCL_DEBUG=""
 
+echo "sudo apt-get update -y"
 sudo apt-get update -y
+echo "sudo apt-get install python3-pip -y"
 sudo apt-get install python3-pip -y
+echo "sudo apt install python3.12-venv -y"
 sudo apt install python3.12-venv -y
+echo "python3 -m venv local_venv"
 python3 -m venv local_venv
-source loacl_venv/bin/activate
+echo "source local_venv/bin/activate"
+source local_venv/bin/activate
+echo "sudo apt-get install git -y"
 sudo apt-get install git -y
 
 pip3 install git+https://github.com/t1101675/transformers@minillm
