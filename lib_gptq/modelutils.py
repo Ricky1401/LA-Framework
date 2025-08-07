887f53b30ab417f294cb4d111a1dc29c42c40cab
Rossetto Riccardo <riccardo.rossetto@virgilio.it>
2025-08-07 22:29:40 +0200

    Update timestamp: 2025-08-07T22:29:40

--- lib_gptq/modelutils.py
+++ lib_gptq/modelutils.py
@@ -14,3 +14,13 @@ def find_layers(module, layers=[nn.Conv2d, nn.Linear], name=''):
             child, layers=layers, name=name + '.' + name1 if name != '' else name1
         ))
     return res
+
+def find_layers_gpt(module, layers=[nn.Conv2d, nn.Linear], name=''):
+    if type(module) in layers:
+        return {name: module}
+    res = {}
+    for (name1, child) in module.named_children():
+        res.update(find_layers(
+            child, layers=layers, name=name + '.' + name1 if name != '' else name1
+        ))
+    return res
