d1648d04292633ba12761a154e5008dbcd43969d
Rossetto Riccardo <riccardo.rossetto@virgilio.it>
2025-08-07 22:36:51 +0200

    Update timestamp: 2025-08-07T22:36:51

--- lib_gptq/modelutils.py
+++ lib_gptq/modelutils.py
@@ -16,11 +16,16 @@ def find_layers(module, layers=[nn.Conv2d, nn.Linear], name=''):
     return res
 
 def find_layers_gpt(module, layers=[nn.Conv2d, nn.Linear], name=''):
-    if type(module) in layers:
-        return {name: module}
     res = {}
-    for (name1, child) in module.named_children():
-        res.update(find_layers(
-            child, layers=layers, name=name + '.' + name1 if name != '' else name1
-        ))
+
+    # Match if module is an instance of any specified type
+    if isinstance(module, tuple(layers)):
+        res[name] = module
+
+    # Recursively check children
+    for name1, child in module.named_children():
+        full_name = f"{name}.{name1}" if name else name1
+        res.update(find_layers_gpt(child, layers=layers, name=full_name))
+
     return res
+
