09185e563ec37b797a22ec8472ffacb0155897a5
Rossetto Riccardo <riccardo.rossetto@virgilio.it>
2025-08-07 18:58:55 +0200

    Update timestamp: 2025-08-07T18:58:55

--- lib_gptq/datautils.py
+++ lib_gptq/datautils.py
@@ -88,6 +88,8 @@ def get_c4(nsamples, seed, seqlen, model):
             tmp = tokenizer(valdata[i]['text'], return_tensors='pt')
             if tmp.input_ids.shape[1] >= seqlen:
                 break
+        
+        print(i, tmp.input_ids.shape)
         if tmp.input_ids.shape[1] - seqlen - 1 <= 0:
             print(i, tmp.input_ids.shape)
             input("Enter to continue:")
